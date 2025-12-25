b51597bb03aabf7268281833433baeff3cf855b5
Rohit Joshi <rohitjoshi05.np@gmail.com>
2025-12-25 08:25:33 +0545

    Initial Commit

---
+++ Calculator.java
@@ -0,0 +1,20 @@
+package week7;
+
+
+/**
+ *  This is a calculator class.
+ *  @author Rohit Joshi
+ *  @version 1.0
+ */
+
+class Calculator {
+	/**
+	 * This is my add method
+	 * @param a First Number
+	 * @param b Second Number
+	 * @return Sum of two numbers
+	 */
+	public int add(int a, int b) {
+		return a + b;
+	}
+}
